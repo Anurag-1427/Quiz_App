a3fd13ebc17c4f5f3e8295183dbfbefc6c6484fb
Anurag Sharma <kaushikanurag2001@gmail.com>
2024-11-24 21:42:45 +0530

    changes regarding api issue

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -27,13 +27,16 @@ const SignUp = () => {
     try {
       setLoading(true);
       setError(false);
-      const res = await fetch("/api/auth/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+      const res = await fetch(
+        `${import.meta.env.BASE_URL}/api/auth/signup`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
       const data = await res.json();
       console.log(data);
       setLoading(false);
