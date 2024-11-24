f86956996a37df6ce59325e8111cc1c86e47734f
Anurag Sharma <kaushikanurag2001@gmail.com>
2024-11-24 22:53:15 +0530

    changes updated

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -83,7 +83,7 @@ const Profile: React.FC = () => {
     e.preventDefault();
     try {
       dispatch(updateUserStart());
-      const res = await fetch(`${import.meta.env.VITE_BACKEND_BASE_URL}/api/user/update/${currentUser?._id}`, {
+      const res = await fetch(`${import.meta.env.VITE_BACKEND_BASE_URL}/api/user/update/${currentUser!._id}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -113,7 +113,7 @@ const Profile: React.FC = () => {
   const handleDeleteAccount = async () => {
     try {
       dispatch(deleteUserStart());
-      const res = await fetch(`${import.meta.env.VITE_BACKEND_BASE_URL}/api/user/delete/${currentUser?._id}`, {
+      const res = await fetch(`${import.meta.env.VITE_BACKEND_BASE_URL}/api/user/delete/${currentUser!._id}`, {
         method: "DELETE",
       });
       const data = await res.json();
