3d346eb7f7e6171691e47192e8ad839332d85e1c
Anurag Sharma <kaushikanurag2001@gmail.com>
2024-11-24 17:45:45 +0530

    feat: cookieParser dependencies added

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -9,6 +12,22 @@ connectDB(mongoURI);
 
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+
 app.listen(3000, () => {
   console.log(`Server listening on port 3000`);
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
 });
