1d384895c6277c36de45dab0f1fb889cec41573e
Anurag Sharma <kaushikanurag2001@gmail.com>
2024-11-28 01:44:07 +0530

    changes updated

--- backend/routes/quiz.route.js
+++ backend/routes/quiz.route.js
@@ -10,10 +10,10 @@ import {
 
 const router = express.Router();
 
-router.post("/start", verifyToken, startTest);
-router.get("/question", verifyToken, question);
-router.post("/answer", verifyToken, submitAnswer);
-router.post("/endTest", verifyToken, endTest);
-router.get("/testHistory", verifyToken, testHistory);
+router.post("/start", startTest);
+router.get("/question", question);
+router.post("/answer", submitAnswer);
+router.post("/endTest", endTest);
+router.get("/testHistory", testHistory);
 
 export default router;
